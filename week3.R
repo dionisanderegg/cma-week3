31b1d1a44b4d3d6935fcf190854ce0fdb5c4871c
Dionis Anderegg <andd@zhaw.ch>
2021-05-07 14:35:49 +0200

    completed task 6

--- week3.R
+++ week3.R
@@ -79,5 +79,79 @@ caro60 %>%
   theme_bw()+
   theme(legend.position = "bottom")
 
+# Import pedestrian.csv
+pedestrian <- read_delim("pedestrian.csv", delim = ",")
 
+# Eplore data
+ggplot(pedestrian, aes(N,E, col = as.factor(TrajID)))+
+  geom_point() +
+  geom_path() +
+  facet_wrap(~TrajID) +
+  theme_bw() +
+  theme(legend.position = "bottom")
 
+ggplot(pedestrian, aes(N,E, col = as.factor(TrajID)))+
+  geom_point() +
+  geom_path() +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+# Use package SimilarityMeasures to analyse the similatity of the trajectories
+library(SimilarityMeasures)
+
+# help(package = "SimilarityMeasures")
+
+# Create matrix for every trajectory without time
+pedestrian.1.m <- as.matrix(filter(pedestrian[,1:3], TrajID == 1))
+pedestrian.2.m <- as.matrix(filter(pedestrian[,1:3], TrajID == 2))
+pedestrian.3.m <- as.matrix(filter(pedestrian[,1:3], TrajID == 3))
+pedestrian.4.m <- as.matrix(filter(pedestrian[,1:3], TrajID == 4))
+pedestrian.5.m <- as.matrix(filter(pedestrian[,1:3], TrajID == 5))
+pedestrian.6.m <- as.matrix(filter(pedestrian[,1:3], TrajID == 6))
+
+# Calculate the Measures and store the results in a DF
+Sililarity <- data.frame(ID = seq(1:6), 
+                         DTW = c(DTW(pedestrian.1.m, pedestrian.1.m),
+                                 DTW(pedestrian.1.m, pedestrian.2.m),
+                                 DTW(pedestrian.1.m, pedestrian.3.m),
+                                 DTW(pedestrian.1.m, pedestrian.4.m),
+                                 DTW(pedestrian.1.m, pedestrian.5.m),
+                                 DTW(pedestrian.1.m, pedestrian.6.m)),
+                         EditDist = c(EditDist(pedestrian.1.m, pedestrian.1.m),
+                                      EditDist(pedestrian.1.m, pedestrian.2.m),
+                                      EditDist(pedestrian.1.m, pedestrian.3.m),
+                                      EditDist(pedestrian.1.m, pedestrian.4.m),
+                                      EditDist(pedestrian.1.m, pedestrian.5.m),
+                                      EditDist(pedestrian.1.m, pedestrian.6.m)),
+                         Frechet = c(Frechet(pedestrian.1.m, pedestrian.1.m),
+                                     Frechet(pedestrian.1.m, pedestrian.2.m),
+                                     Frechet(pedestrian.1.m, pedestrian.3.m),
+                                     Frechet(pedestrian.1.m, pedestrian.4.m),
+                                     Frechet(pedestrian.1.m, pedestrian.5.m),
+                                     Frechet(pedestrian.1.m, pedestrian.6.m)),
+                         LCSS = c(LCSS(pedestrian.1.m, pedestrian.1.m, pointDistance = 20, pointSpacing = 0, errorMarg = 1),
+                                  LCSS(pedestrian.1.m, pedestrian.2.m, pointDistance = 20, pointSpacing = 0, errorMarg = 1),
+                                  LCSS(pedestrian.1.m, pedestrian.3.m, pointDistance = 20, pointSpacing = 0, errorMarg = 1),
+                                  LCSS(pedestrian.1.m, pedestrian.4.m, pointDistance = 20, pointSpacing = 0, errorMarg = 1),
+                                  LCSS(pedestrian.1.m, pedestrian.5.m, pointDistance = 20, pointSpacing = 0, errorMarg = 1),
+                                  LCSS(pedestrian.1.m, pedestrian.6.m, pointDistance = 20, pointSpacing = 0, errorMarg = 1)))
+# Question @NILS : See warning message when executing lines above. Warning message:
+#    In Frechet(pedestrian.1.m, pedestrian.3.m) :
+#     The Frechet distance was unable to be found
+# What could be the reason for this error? Result is -1 
+
+# Long-Format for easyer plotting
+Sililarity_long <- gather(Sililarity, Measure, Value, DTW : LCSS)
+
+# Plot results
+ggplot(Sililarity_long, aes(as.factor(ID), Value, fill = as.factor(ID))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~Measure, scales = "free_y") +
+  theme_bw() +
+  theme(legend.position = "none") +
+  labs(title = "Computed similarities using different measures \n between trajectory 1 and all the others",
+       y = "Value\n", x = "\nComparison trajectory")
+
+# ATTENTION: LCSS strongly depends on pointDistance and errorMarg!
+#  Different caluclation times
+#  Different RESULTS!
